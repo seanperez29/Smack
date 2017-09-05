825d1ae3442d3ea40f007d76f57058b48e6b42de
Sean Perez <sean.perez29@gmail.com>
2017-09-05 14:23:07 -0600

    feat: enable timestamp displaying for messages

--- Smack/View/MessageCell.swift
+++ Smack/View/MessageCell.swift
@@ -20,6 +20,17 @@ class MessageCell: UITableViewCell {
         messageBodyLabel.text = message.messageBody
         usernameLabel.text = message.userName
         userImage.backgroundColor = UserDataService.instance.returnUIColor(message.userAvatarColor)
+        var isoDate = message.timeStamp
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = isoDate.substring(to: end)
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm a"
+        if let finalDate = chatDate {
+            let finalDate = newFormatter.string(from: finalDate)
+            timeStampLabel.text = finalDate
+        }
     }
 
 }
